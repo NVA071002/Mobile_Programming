37ea37ce65a71d8cbe6c1fc9901f2906f3e74994
NVA071002 <20110352@student.hcmute.edu.vn>
2023-03-13 15:20:15 +0700

    Initial commit

---
+++ Week3/AppResources/Lab2/app/build/generated/source/buildConfig/debug/com/example/sumof2number/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.sumof2number;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.sumof2number";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
