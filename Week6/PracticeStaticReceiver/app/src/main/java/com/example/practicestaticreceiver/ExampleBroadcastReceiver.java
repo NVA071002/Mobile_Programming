ec878099d136e9f1822db494a59eefa3587ce710
NVA071002 <20110352@student.hcmute.edu.vn>
2023-03-14 14:06:41 +0700

    update14/3

---
+++ Week6/PracticeStaticReceiver/app/src/main/java/com/example/practicestaticreceiver/ExampleBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package com.example.practicestaticreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
